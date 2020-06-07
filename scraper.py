360eb1c9f9dda30f613a745f6bcc25361f6ef6dc
mdaabis <42125154+mdaabis@users.noreply.github.com>
2020-06-07 17:35:30 +0100

    Time refresh implemented to ensure that the price check method is run every 5 seconds while the script is running.

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from twilio.rest import Client
-import time
+import threading
+from decouple import config
+
 
 URL = 'https://www.amazon.co.uk/16-Inch-2-4ghz-8-Core-Deecies-Limited/dp/B0821LJM6M/ref=sr_1_9?dchild=1&keywords=macbook&qid=1591501857&sr=8-9'
 
@@ -9,6 +11,10 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 
 
 def price_check():
+
+    # Repeat price check function every 5 seconds
+    threading.Timer(5.0, price_check).start()
+
     page = requests.get(URL, headers=headers)
 
     soup1 = BeautifulSoup(page.content, 'html.parser')
@@ -17,39 +23,33 @@ def price_check():
     soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
 
     price = soup2.find(id='priceblock_ourprice').get_text()
+    # print(price)
 
     int_price = float(price[1:].replace(',',''))
+    # print(int_price)
 
     # Value at which you want to be notified if the product becomes available for this price or cheaper
     threshold_price = 35000
-
-    print("1")
     
     if (threshold_price > int_price):
         send_SMS(int_price)
-        print("2")
 
 
 
 
 def send_SMS(price):
-    print('executed')
-    account_sid = 'AC7c94e3a7afb227710a9cac82c385a0c8'
-    auth_token = '79bdbc8f5bacda4a140a46b4928cdf9f'
+    account_sid = config("TWILIO_ACCOUNT_SID")
+    auth_token = config("TWILIO_AUTH_TOKEN")
     
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        to = '07885511334',
-        from_ = '+441527962157',
-        body = 'Price is lower than threshold'
+        to = config('TO_NUMBER'),
+        from_ = config('FROM_NUMBER'),
+        body = 'Price is lower than '
     )
 
-
-while(True):
-    price_check()
-    # Time (in seconds) that you want to wait between checking price again
-    time.sleep(60*60)
+price_check()
 
 
 
