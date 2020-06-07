6174eb8c8aaa869db222b2983594f684a6174492
mdaabis <42125154+mdaabis@users.noreply.github.com>
2020-06-07 05:08:22 +0100

    Get price from Amazon page

--- scraper.py
+++ scraper.py
@@ -1,14 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.amazon.co.uk/adidas-Manchester-United-Jersey-Large/dp/B07R2B8GTF/ref=sr_1_5?dchild=1&keywords=manchester+united+top&qid=1591499606&sr=8-5'
+URL = 'https://www.amazon.co.uk/16-Inch-2-4ghz-8-Core-Deecies-Limited/dp/B0821LJM6M/ref=sr_1_9?dchild=1&keywords=macbook&qid=1591501857&sr=8-9'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
 
-page = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+def price_check():
+    page = requests.get(URL, headers=headers)
 
-price = soup.find(id='priceblock_ourprice')
+    soup1 = BeautifulSoup(page.content, 'html.parser')
 
-print(price)
+    # Using a second soup allows me to add an extra level of indirection and bypasses the fact that Amazon makes its HTML code with JavaScript
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+    price = soup2.find(id='priceblock_ourprice').get_text()
+
+    int_price = float(price[1:].replace(',',''))
+
